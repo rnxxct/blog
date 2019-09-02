bd95b171a11a806772cd74461589b604193bc2dd
srcheke <nxren@jouletek.com>
2019-09-02 22:05:11 +0800

    init

---
+++ article/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+from .models import ArticlePost
+# 视图函数
+def article_list(request):
+    articles = ArticlePost.objects.all()
+    # 需要传递给模板（templates）的对象
+    context = { 'articles': articles }
+    # render函数：载入模板，并返回context对象
+    return render(request, 'article/list.html', context)
+
+def article_detail(request, id):
+    # 取出相应的文章
+    article = ArticlePost.objects.get(id=id)
+    # 需要传递给模板的对象
+    context = { 'article': article }
+    # 载入模板，并返回context对象
+    return render(request, 'article/detail.html', context)
