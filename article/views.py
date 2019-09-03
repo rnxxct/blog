b8672d9db1408204b78b1606baf889ed75426115
srcheke <nxren@jouletek.com>
2019-09-03 13:51:25 +0800

    'markdown'

--- article/views.py
+++ article/views.py
@@ -3,18 +3,35 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 from .models import ArticlePost
+import markdown
+
+
 # 视图函数
 def article_list(request):
     articles = ArticlePost.objects.all()
+    for article in articles:
+        article.body = markdown.markdown(article.body, extensions=[
+            # 包含 缩写、表格等常用扩展
+            'markdown.extensions.extra',
+            # 语法高亮扩展
+            'markdown.extensions.codehilite',
+        ])
     # 需要传递给模板（templates）的对象
-    context = { 'articles': articles }
+    context = {'articles': articles}
     # render函数：载入模板，并返回context对象
     return render(request, 'article/list.html', context)
+
 
 def article_detail(request, id):
     # 取出相应的文章
     article = ArticlePost.objects.get(id=id)
+    article.body = markdown.markdown(article.body, extensions=[
+        # 包含 缩写、表格等常用扩展
+        'markdown.extensions.extra',
+        # 语法高亮扩展
+        'markdown.extensions.codehilite',
+    ])
     # 需要传递给模板的对象
-    context = { 'article': article }
+    context = {'article': article}
     # 载入模板，并返回context对象
     return render(request, 'article/detail.html', context)
