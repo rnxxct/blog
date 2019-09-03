d0577abdd14a1da70526c0128c30719fb1c6f419
srcheke <nxren@jouletek.com>
2019-09-03 16:26:57 +0800

    'CURD'

--- article/views.py
+++ article/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import ArticlePost
 import markdown
+from .forms import ArticlePostForm
+from django.contrib.auth.models import User
 
 
 # 视图函数
@@ -35,3 +35,64 @@ def article_detail(request, id):
     context = {'article': article}
     # 载入模板，并返回context对象
     return render(request, 'article/detail.html', context)
+
+
+def article_create(request):
+    # 判断用户是否提交数据
+    if request.method == "POST":
+        # 将提交的数据赋值到表单实例中
+        print(request.POST)
+        article_post_form = ArticlePostForm(data=request.POST)
+        print(article_post_form)
+        # 判断提交的数据是否满足模型的要求
+        if article_post_form.is_valid():
+            # 保存数据，但暂时不提交到数据库中
+            new_article = article_post_form.save(commit=False)
+            # 指定数据库中 id=1 的用户为作者
+            # 如果你进行过删除数据表的操作，可能会找不到id=1的用户
+            # 此时请重新创建用户，并传入此用户的id
+            new_article.author = User.objects.get(id=1)
+            # 将新文章保存到数据库中
+            new_article.save()
+            # 完成后返回到文章列表
+            return redirect("article:article_list")
+        # 如果数据不合法，返回错误信息
+        else:
+            return HttpResponse("表单内容有误，请重新填写。")
+    # 如果用户请求获取数据
+    else:
+        # 创建表单类实例
+        article_post_form = ArticlePostForm()
+        # 赋值上下文
+        context = {'article_post_form': article_post_form}
+        # 返回模板
+        return render(request, 'article/create.html', context)
+
+
+def article_delete(request, id):
+    print(request)
+    print(id)
+    ArticlePost.objects.get(id=id).delete()
+    return redirect("article:article_list")
+
+
+def article_update(request, id):
+    article = ArticlePost.objects.get(id=id)
+    if request.method == "POST":
+        # 将提交的数据赋值到表单实例中
+        print(id)
+        article_post_form = ArticlePostForm(data=request.POST)
+        print(article_post_form.fields)
+        if article_post_form.is_valid():
+            article.title = request.POST['title']
+            article.body = request.POST['body']
+            article.save()
+            # 完成后返回到文章列表
+            return redirect("article:article_detail", id=id)
+        # 如果数据不合法，返回错误信息
+        else:
+            return HttpResponse("表单内容有误，请重新填写。")
+        # 如果用户请求获取数据
+    else:
+        context = {'article': article}
+        return render(request, 'article/edit.html', context)
